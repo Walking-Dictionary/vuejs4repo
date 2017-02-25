a435fc45c65f61b1bcd45d722b76701f7d3a43f7
Walking-Dictionary <gdande@cytonn.com>
2017-02-25 06:51:35 -0500

    vuejs Routing

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <main-layout>
+    <p> Second Route</p>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
